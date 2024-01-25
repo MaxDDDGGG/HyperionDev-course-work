12e36103737816fc2f915538d78b0515bf88d22f
Max <m.griffiths22@outlook.com>
2024-01-25 13:26:09 +0000

    Added a while loop to check for errors and added  comments

--- T15 - IO Operations - Output/student_register.py
+++ T15 - IO Operations - Output/student_register.py
@@ -1,8 +1,24 @@
-number_of_students = int(input("Please enter the number of students you are registering: "))
+'''input for the number of students and error checker'''
+number_of_students = input("Please enter the number of students you are registering: ")
+while True:
+    try:
+        number_of_students = int(number_of_students)
+        break
+    except ValueError:
+        print("\n---ERROR---\nIncorrect input: Number of children must be a whole number!")
+        number_of_students = input("Please enter the number of students you are registering: ")
+    
+'''Create a blank dictionary 
+Add the student IDs'''
 added_ids = []
 while number_of_students > 0:
     student_ID = input("Enter student ID: ")
 
+    '''The dictionary is them appended to reg_form.txt
+    Unless it is already present in reg_form.txt, then an error message is printed
+    If it is added correctly a success message is printed, containing the student ID
+    For each student number successfully added the number_of_students is counted down until number_of_students = 0
+    When all students are added, there is a prompt to say "All Students addded!"'''
     with open("reg_form.txt", "a+") as f:
         if student_ID in added_ids:
             print("Error! Student already added to register")
